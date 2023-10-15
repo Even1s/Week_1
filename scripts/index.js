86ea40622aace5f034fe925a4a1962b3fa207701
Egor <eockunev@yandex.ru>
2023-10-16 03:11:25 +0700

    3 day

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,9 @@
 const burger = document.querySelector("#menu-hamburger");
 const mobHeader = document.querySelector(".mobile-header");
 const burgerImg = burger.querySelector("img");
-const body = document.querySelector("body");
 burger.onclick = (e) =>{
     mobHeader.classList.toggle('mobile-header_open');
-    body.classList.toggle('lock');
+    document.querySelector("body").classList.toggle('lock');
     if(burgerImg.getAttribute('src')=="/img/menu-hamburger.svg") {
         burgerImg.setAttribute('src', '/img/menu-close.svg');
 } else { burgerImg.setAttribute('src', '/img/menu-hamburger.svg') }
