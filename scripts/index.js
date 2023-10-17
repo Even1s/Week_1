3f0fb7dbd31709daf252ee8199bc76394209796e
Egor <eockunev@yandex.ru>
2023-10-18 02:36:41 +0700

    4 day

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 const burger = document.querySelector("#menu-hamburger");
-const mobHeader = document.querySelector(".mobile-header");
-const burgerImg = burger.querySelector("img");
+const mobileHeader = document.querySelector(".mobile-header");
+const burgerImg = burger.querySelector("img"); // img это не класс, это блок img внутри кнопки бургера (открытие меню), всегда будет один
 burger.onclick = () =>{
-    mobHeader.classList.toggle('mobile-header_open');
-    document.querySelector("body").classList.toggle('lock');
-    if(document.querySelector("body").classList.contains('lock')) {
+    mobileHeader.classList.toggle('mobile-header_open');
+    document.body.classList.toggle('lock');
+    if(document.body.classList.contains('lock')) {
         burgerImg.setAttribute('src', '/img/menu-close.svg');
     } else {
         burgerImg.setAttribute('src', '/img/menu-hamburger.svg')
